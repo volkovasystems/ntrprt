189014c643c580cbabe588f4e2079544103935ee
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-26 14:29:26 +0800

    Updated test.

--- ntrprt-test.js
+++ ntrprt-test.js
@@ -5,8 +5,8 @@ const ntrprt = require( "./ntrprt.js" );
 let data = { };
 data[ Symbol( "hello" ) ] = 12345;
 
-assert.equal( ntrprt( Symbol( "hello" ), data ), 12345, "should be equal" );
+assert.equal( ntrprt( Symbol( "hello" ), data ), 12345, "should be equal to 12345" );
 
-assert.equal( ntrprt( "hello", data ), 12345, "should be equal" );
+assert.equal( ntrprt( "hello", data ), 12345, "should be equal to 12345" );
 
 console.log( "ok" );
